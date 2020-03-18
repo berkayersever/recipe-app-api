935cdffbd660d85ceefc1529c16d416366357583
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-18 22:52:59 +0300

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
+
     def test_create_user_with_email_successful(self):
         """Tests creating a new user with an email is successful"""
         email = 'test@live.se'
@@ -16,3 +17,8 @@ class ModelTests(TestCase):
         email = 'test@LIVE.SE'
         user = get_user_model().objects.create_user(email, 'Test1234')
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'Test1234')
