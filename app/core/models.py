69ec0deebd254ab88e871e7eaa4f602e555b4443
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-18 22:42:02 +0300

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -9,3 +9,13 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self.db)
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
