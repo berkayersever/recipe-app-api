54a73c17b7800d1e2118b751dd04835b24dd289a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-19 04:26:32 +0300

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
         user = self.create_user(email, password)
-        user.is_active = True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
