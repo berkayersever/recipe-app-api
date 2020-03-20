eb4e8318ef9541f97e05ebb539b4418ea760d921
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-21 00:44:48 +0300

    Update test_user_api.py

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,7 +32,11 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Tests creating an user that already exists fails"""
-        payload = {'email': 'test@live.se', 'password': 'Test1234'}
+        payload = {
+            'email': 'test@live.se',
+            'password': 'Test1234',
+            'name': 'Test',
+        }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
