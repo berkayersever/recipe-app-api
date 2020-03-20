2064a4b4f470632ac523378275e4b8ad5526d044
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-21 00:40:13 +0300

    Minor change in test_user_api.py

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -39,7 +39,11 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Tests that the password must be more than 5 characters"""
-        payload = {'email': 'test@live.se', 'password': 'Test'}
+        payload = {
+            'email': 'test@live.se',
+            'password': 'Test',
+            'name': 'Test',
+        }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
