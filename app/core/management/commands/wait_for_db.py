df438b78f42887816ae4b78acf53fba0a8c05dd8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-21 00:35:59 +0300

    Minor change in wait_for_db.py

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,6 +14,8 @@ class Command(BaseCommand):
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database is unavailable, waiting for one second...')
+                self.stdout.write(
+                    'Database is unavailable, waiting for one second...'
+                )
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database is available!'))
