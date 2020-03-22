b66e572f67a5f7a3d24b33e56a4de46d83c00574
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-22 18:21:36 +0300

    Update views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,6 +5,22 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
+class BaseRecipeAttributeViewSet(viewsets.GenericViewSet,
+                                 mixins.ListModelMixin,
+                                 mixins.CreateModelMixin):
+    """Base ViewSet for user owned recipe attributes"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Returns objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Creates a new object"""
+        serializer.save(user=self.request.user)
+
+
 class TagViewSet(viewsets.GenericViewSet,
                  mixins.ListModelMixin,
                  mixins.CreateModelMixin):
