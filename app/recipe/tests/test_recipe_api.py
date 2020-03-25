47ac9f61a79b4f5a13d3261c718c5bea741d073f
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-25 06:40:17 +0300

    Modified the test_retrieve_recipes method.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -45,10 +45,10 @@ class PrivateRecipeApiTests(TestCase):
 
     def test_retrieve_recipes(self):
         """Tests retrieving a list of recipes"""
-        sample_recipe(user=self.user)
-        sample_recipe(user=self.user)
+        sample_recipe(user=self.user, title='Sample Recipe - 1')
+        sample_recipe(user=self.user, title='Sample Recipe - 2')
         res = self.client.get(RECIPES_URL)
-        recipes = Recipe.objects.all().order_by('-id')
+        recipes = Recipe.objects.all().order_by('id')
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
