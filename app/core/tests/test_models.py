35f9c2182da18dab31c930b8c35dfb5408fe6d63
Berkay Ersever <berkayersever@sabanciuniv.edu>
2020-03-26 06:22:15 +0300

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from core import models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from unittest.mock import patch
 
 
 def sample_user(email='test@live.se', password='Test1234Pass'):
@@ -65,3 +66,12 @@ class ModelTests(TestCase):
             price=5.00
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Tests that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'my-image.jpg')
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
